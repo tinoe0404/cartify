596b1bc42331e2b05db19d9b4b88e8a6a90bf546
tinoe0404 <tinoe044@gmail.com>
2025-10-07 15:08:14 +0200

    initial commit - project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send('Signup endpoint');
+};
+
+export const login = (req, res) => {
+  res.send('Login endpoint');
+};
+
+export const logout = (req, res) => {
+  res.send('Logout endpoint');
+};
