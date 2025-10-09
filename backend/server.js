87e5862f911d32b560845a6cb7bbe2d95b93cfd4
tinoe0404 <tinoe044@gmail.com>
2025-10-09 15:20:34 +0200

    created the product model

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js'; // ✅ correct now
+import productRoutes from './routes/product.route.js'; 
 import { connectDB } from './lib/db.js';
-
-
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // allows you to parse the body of the request
+app.use(express.json());
 app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
