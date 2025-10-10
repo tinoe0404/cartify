b66c1ed8fdaf9ea338784fc7dd4578a6b2dc0904
tinoe0404 <tinoe044@gmail.com>
2025-10-10 10:44:15 +0200

    created the getRecommendationProducts

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -93,3 +93,29 @@ export const deleteProduct = async (req, res) => {
 		res.status(500).json({ message: "Server error", error: error.message });
 	}
 };
+
+//creating the getRecommendedProducts
+export const getRecommendedProducts = async (req, res) => {
+	try {
+		const products = await Product.aggregate([
+			{
+				$sample: { size: 4 },
+			},
+			{
+				$project: {
+					_id: 1,
+					name: 1,
+					description: 1,
+					image: 1,
+					price: 1,
+				},
+			},
+		]);
+
+		res.json(products);
+	} catch (error) {
+		console.log("Error in getRecommendedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
