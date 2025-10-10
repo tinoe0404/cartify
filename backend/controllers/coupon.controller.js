d397738c88ffcbd0ba30f24e82df490b5c980243
tinoe0404 <tinoe044@gmail.com>
2025-10-10 12:34:13 +0200

    create the getCoupon functionality and created the coupon touter file code

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,11 @@
+import Coupon from "../models/coupon.model.js";
+
+export const getCoupon = async (req, res) => {
+	try {
+		const coupon = await Coupon.findOne({ userId: req.user._id, isActive: true });
+		res.json(coupon || null);
+	} catch (error) {
+		console.log("Error in getCoupon controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
