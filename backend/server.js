0270a0efd2a68f08371ba169cb990fa1a65592a5
tinoe0404 <tinoe044@gmail.com>
2025-10-10 15:15:23 +0200

    create payment route, stripe.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js'; // ✅ correct now
 import productRoutes from './routes/product.route.js'; 
 import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -19,6 +20,8 @@ app.use('/api/auth', authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
