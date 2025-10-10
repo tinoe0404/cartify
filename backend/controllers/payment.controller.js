1e3196d551372d3eed503b4b9a4013d236490c51
tinoe0404 <tinoe044@gmail.com>
2025-10-10 15:52:47 +0200

    create createNewCoupon functionality

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -85,3 +85,19 @@ async function createStripeCoupon(discountPercentage) {
 	return coupon.id;
 }
 
+
+// create createNewCoupon
+async function createNewCoupon(userId) {
+	await Coupon.findOneAndDelete({ userId });
+
+	const newCoupon = new Coupon({
+		code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+		discountPercentage: 10,
+		expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+		userId: userId,
+	});
+
+	await newCoupon.save();
+
+	return newCoupon;
+}
