9a8eb3ae06341e98aaf244a4c6f557344e036b78
tinoe0404 <tinoe044@gmail.com>
2025-10-10 12:04:42 +0200

    created getCartProducts functionality

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,5 +1,23 @@
 import Product from "../models/product.model";
 
+//create getCartProducts controller function 
+export const getCartProducts = async (req, res) => {
+	try {
+		const products = await Product.find({ _id: { $in: req.user.cartItems } });
+
+		// add quantity for each product
+		const cartItems = products.map((product) => {
+			const item = req.user.cartItems.find((cartItem) => cartItem.id === product.id);
+			return { ...product.toJSON(), quantity: item.quantity };
+		});
+
+		res.json(cartItems);
+	} catch (error) {
+		console.log("Error in getCartProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
 // create addToCart controller function 
 export const addToCart = async (req, res) => {
 	try {
