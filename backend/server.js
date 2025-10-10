c2fa2f6d776e9ce8823177f14119f5ab0c0ca119
tinoe0404 <tinoe044@gmail.com>
2025-10-10 11:39:55 +0200

    created respective cartRoute routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js'; // ✅ correct now
 import productRoutes from './routes/product.route.js'; 
+import cartRoutes from './routes/cart.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
