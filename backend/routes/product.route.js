149420d7432386ec7e3a30533c838f970763360c
tinoe0404 <tinoe044@gmail.com>
2025-10-10 10:30:39 +0200

    created the createProduct function

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { createProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
-router.get("featured", getFeaturedProducts)
+router.get("featured", getFeaturedProducts);
+routes.post("/", protectRoute, adminRoute, createProduct)
 
 export default router;
