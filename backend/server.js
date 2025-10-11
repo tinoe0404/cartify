d90aa5c0b9ca38dae41daf41c44da86440ecdf55
tinoe0404 <tinoe044@gmail.com>
2025-10-11 09:42:34 +0200

    create the router for analytics

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
 import paymentRoutes from './routes/payment.route.js';
+import analyticsRoutes from './routes/analytics.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -21,6 +22,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 
 app.listen(PORT, () => {
