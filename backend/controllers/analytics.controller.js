7f5e0946b2933869a7b96ce7b2d9abdb23a3b2a6
tinoe0404 <tinoe044@gmail.com>
2025-10-11 09:52:07 +0200

    create th getAnalyticsData

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,26 +1,29 @@
-import express from "express";
-import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import { getAnalyticsData, getDailySalesData } from "../controllers/analytics.controller.js";
-
-const router = express.Router();
-
-router.get("/", protectRoute, adminRoute, async (req, res) => {
-	try {
-		const analyticsData = await getAnalyticsData();
-
-		const endDate = new Date();
-		const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
-
-		const dailySalesData = await getDailySalesData(startDate, endDate);
-
-		res.json({
-			analyticsData,
-			dailySalesData,
-		});
-	} catch (error) {
-		console.log("Error in analytics route", error.message);
-		res.status(500).json({ message: "Server error", error: error.message });
-	}
-});
-
 export default router;
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
+
+// Function to get overall analytics data
+export const getAnalyticsData = async () => {
+	const totalUsers = await User.countDocuments();
+	const totalProducts = await Product.countDocuments();
+
+	const salesData = await Order.aggregate([
+		{
+			$group: {
+				_id: null, // it groups all documents together,
+				totalSales: { $sum: 1 },
+				totalRevenue: { $sum: "$totalAmount" },
+			},
+		},
+	]);
+
+	const { totalSales, totalRevenue } = salesData[0] || { totalSales: 0, totalRevenue: 0 };
+
+	return {
+		users: totalUsers,
+		products: totalProducts,
+		totalSales,
+		totalRevenue,
+	};
+};
