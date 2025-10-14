a5be28f4135563d75ce17a0533b56dd4f7dabc61
tinoe0404 <tinoe044@gmail.com>
2025-10-14 15:10:12 +0200

    created the toggleFeaturedProduct function

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -36,5 +36,23 @@ export const useProductStore = create((set) => ({
 			toast.error(error.response.data.error || "Failed to fetch products");
 		}
 	},
+
+    // create toggleFeaturedProduct function 
+    toggleFeaturedProduct: async (productId) => {
+		set({ loading: true });
+		try {
+			const response = await axios.patch(`/products/${productId}`);
+			// this will update the isFeatured prop of the product
+			set((prevProducts) => ({
+				products: prevProducts.products.map((product) =>
+					product._id === productId ? { ...product, isFeatured: response.data.isFeatured } : product
+				),
+				loading: false,
+			}));
+		} catch (error) {
+			set({ loading: false });
+			toast.error(error.response.data.error || "Failed to update product");
+		}
+	},
 	
 }));
