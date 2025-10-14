917f30be80b928f7695e3badcd3589247ec59011
tinoe0404 <tinoe044@gmail.com>
2025-10-14 12:34:57 +0200

    designed a createProductForm

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -2,6 +2,10 @@ import { BarChart, PlusCircle, ShoppingBasket } from "lucide-react";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
+import AnalyticsTab from "../components/AnalyticsTab";
+import CreateProductForm from "../components/CreateProductForm";
+import ProductsList from "../components/ProductsList";
+//import { useProductStore } from "../stores/useProductStore"; 
 
 const tabs = [
 	{ id: "create", label: "Create Product", icon: PlusCircle },
@@ -11,7 +15,11 @@ const tabs = [
 
 const AdminPage = () => {
 	const [activeTab, setActiveTab] = useState("create");
-	
+	// const { fetchAllProducts } = useProductStore();
+
+	/*useEffect(() => {
+		fetchAllProducts();
+	}, [fetchAllProducts]);*/
 
 	return (
 		<div className='min-h-screen relative overflow-hidden'>
