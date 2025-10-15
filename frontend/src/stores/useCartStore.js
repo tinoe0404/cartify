5378dcb555fa37ee4fdb48f3484cc28e34d6f52e
tinoe0404 <tinoe044@gmail.com>
2025-10-15 10:39:33 +0200

    created the calculateTotals function in useProducttStore

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -46,5 +46,19 @@ export const useCartStore = create((set, get) => ({
 			toast.error(error.response.data.message || "An error occurred");
 		}
 	},
+
+    // create calculateTotals fucntion 
+    calculateTotals: () => {
+		const { cart, coupon } = get();
+		const subtotal = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+		let total = subtotal;
+
+		if (coupon) {
+			const discount = subtotal * (coupon.discountPercentage / 100);
+			total = subtotal - discount;
+		}
+
+		set({ subtotal, total });
+	},
 	
 }));
