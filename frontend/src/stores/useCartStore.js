7fd09fefaf99ff15bc1279597adaaf7a728edeb3
tinoe0404 <tinoe044@gmail.com>
2025-10-15 10:18:26 +0200

    created the addToCart function in useCartStore

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -14,13 +14,35 @@ export const useCartStore = create((set, get) => ({
 	total: 0,
 	subtotal: 0,
 
-	getCartItems: async () => {
+	// create getCartItems function
+    getCartItems: async () => {
 		try {
 			const res = await axios.get("/cart");
 			set({ cart: res.data });
 			get().calculateTotals();
 		} catch (error) {
 			set({ cart: [] });
+			toast.error(error.response.data.message || "An error occurred");
+		}
+	},
+
+    // create addToCart function 
+    addToCart: async (product) => {
+		try {
+			await axios.post("/cart", { productId: product._id });
+			toast.success("Product added to cart");
+
+			set((prevState) => {
+				const existingItem = prevState.cart.find((item) => item._id === product._id);
+				const newCart = existingItem
+					? prevState.cart.map((item) =>
+							item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
+					  )
+					: [...prevState.cart, { ...product, quantity: 1 }];
+				return { cart: newCart };
+			});
+			get().calculateTotals();
+		} catch (error) {
 			toast.error(error.response.data.message || "An error occurred");
 		}
 	},
