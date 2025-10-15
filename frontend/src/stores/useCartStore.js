6c5d308042e95ce9e6104b62e19d7704d88782b7
tinoe0404 <tinoe044@gmail.com>
2025-10-15 10:14:57 +0200

    created the getCartItems function in useCartStore

---
+++ frontend/src/stores/useCartStore.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { useCartStore } from "../stores/useCartStore";
+import { motion } from "framer-motion";
+import { ShoppingCart } from "lucide-react";
+import CartItem from "../components/CartItem";
+import PeopleAlsoBought from "../components/PeopleAlsoBought";
+import OrderSummary from "../components/OrderSummary";
+import GiftCouponCard from "../components/GiftCouponCard";
+
+export const useCartStore = create((set, get) => ({
+	cart: [],
+	coupon: null,
+	total: 0,
+	subtotal: 0,
+
+	getCartItems: async () => {
+		try {
+			const res = await axios.get("/cart");
+			set({ cart: res.data });
+			get().calculateTotals();
+		} catch (error) {
+			set({ cart: [] });
+			toast.error(error.response.data.message || "An error occurred");
+		}
+	},
+	
+}));
