c093ca723ee76e6494f2b93ea92b692d9d28dafc
tinoe0404 <tinoe044@gmail.com>
2025-10-17 10:51:47 +0200

    create the FeaturedProducts functionality

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -3,7 +3,34 @@ import { ShoppingCart, ChevronLeft, ChevronRight } from "lucide-react";
 import { useCartStore } from "../stores/useCartStore";
 
 const FeaturedProducts = ({ featuredProducts }) => {
-	
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [itemsPerPage, setItemsPerPage] = useState(4);
+
+	const { addToCart } = useCartStore();
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth < 640) setItemsPerPage(1);
+			else if (window.innerWidth < 1024) setItemsPerPage(2);
+			else if (window.innerWidth < 1280) setItemsPerPage(3);
+			else setItemsPerPage(4);
+		};
+
+		handleResize();
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	const nextSlide = () => {
+		setCurrentIndex((prevIndex) => prevIndex + itemsPerPage);
+	};
+
+	const prevSlide = () => {
+		setCurrentIndex((prevIndex) => prevIndex - itemsPerPage);
+	};
+
+	const isStartDisabled = currentIndex === 0;
+	const isEndDisabled = currentIndex >= featuredProducts.length - itemsPerPage;
 
 	return (
 		<div className='py-12'>
