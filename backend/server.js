beffe1abcb528143535c03d4375ebf28f2f85d08
tinoe0404 <tinoe044@gmail.com>
2025-10-17 15:13:43 +0200

    deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/auth.route.js'; // ✅ correct now
 import productRoutes from './routes/product.route.js'; 
 import cartRoutes from './routes/cart.route.js';
@@ -14,6 +15,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({limit : "50mb"}));
 app.use(cookieParser());
 
@@ -24,6 +27,13 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
